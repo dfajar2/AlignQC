25e019d1def69a83f552e53eb206b4b7f053b26c
Jason Weirather <jason-weirather@users.noreply.github.com>
2017-07-04 09:50:05 -0400

    remove index and compare modes as the former is overly complicated and the later is not implemented

--- alignqc/alignqc.py
+++ alignqc/alignqc.py
@@ -8,19 +8,17 @@ import dump
 import compare
 from seqtools.cli.utilities.bam_bgzf_index import external_cmd as index_bam
 
-version = '1.3dev'
+version = '1.3'
 
 def main():
   operable_argv = [sys.argv[0]]+sys.argv[2:]
-  sys.argv = sys.argv[:2]  
+  sys.argv = sys.argv[:2]
   #do our inputs
   args = do_inputs()
   if args.mode == 'analyze':
     analyze.external_cmd(" ".join(operable_argv),version=version)
   elif args.mode == 'dump':
     dump.external_cmd(" ".join(operable_argv),version=version)
-  elif args.mode == 'index':
-    index_bam(" ".join(operable_argv))
   elif args.mode == 'compare':
     compare.external_cmd(" ".join(operable_argv),version=version)
   else:
@@ -30,7 +28,7 @@ def do_inputs():
   # Setup command line inputs
   global version
   parser=argparse.ArgumentParser(description="Version "+str(version)+"\nReview reports about alignments.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('mode',choices=['analyze','dump','compare','combine','index'],help="MODE of program to run")
+  parser.add_argument('mode',choices=['analyze','dump','compare'],help="MODE of program to run")
   args = parser.parse_args()
   return args
 
